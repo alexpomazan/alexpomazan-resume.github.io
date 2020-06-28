b5b0875f1264277e2dcfa4fd873b68a0be9fdcf0
Aleksey Pomazan <aleksey.pomazan@gmail.com>
2020-06-29 02:31:33 +0300

    add skills and some fixes

--- script.js
+++ script.js
@@ -10,10 +10,16 @@ $(document).ready(function () {
                 $('#wrapper').removeClass('nav-fixed');
             }
     });
-    $("#wrapper").on("click","a", function (event) {
-            event.preventDefault();
-            var id = $(this).attr('href'),
-                top = $(id).offset().top;
-            $('body,html').animate({scrollTop: top}, 1500);
+    $("#nav").on("click","a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 800);
+    });
+    $("#intro").on("click","a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 800);
     });
 });

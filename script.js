de5afda6b7ec3be3ec5e17339d7074966f45f02e
Aleksey Pomazan <aleksey.pomazan@gmail.com>
2020-07-05 17:48:13 +0300

    edit about, fixed nav, edit portfolio

--- script.js
+++ script.js
@@ -23,8 +23,23 @@ $(document).ready(function () {
         $('body,html').animate({scrollTop: top}, 800);
     });
 });
+!function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
+document.addEventListener('DOMContentLoaded', function() {
+    let modalButtons = document.querySelectorAll('.js-open-modal'),
+        overlay      = document.querySelector('#overlay-modal'),
+        closeButtons = document.querySelector('.js-modal-close');
 
-const sum = (a=1, b) => {
-    return b= ++a -5
-}
-console.log(sum())
+
+    modalButtons.forEach(function(item){
+
+        item.addEventListener('click', function(e) {
+
+            e.preventDefault();
+            let modalId = this.getAttribute('data-modal'),
+                modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
+
+            modalElem.classList.add('active');
+            overlay.classList.add('active');
+        }); // end click
+    }); // end foreach
+}); // end ready
